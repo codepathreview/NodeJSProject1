17ca11ddded99d3cdf9f92388d1e588d0047e7df
git-sujit <sujit.cse.nitt@gmail.com>
2016-03-13 23:10:42 -0700

    Server working, npm setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-console.log('Sujit K Singh')
+"use strict";
+console.log('Starting the server')
 let http = require('http')
 
 http.createServer((req, res) => {
-    console.log('Request received at: ${req.url})
+    console.log('Request received at: ${req.url} ')
     res.end('hello world\n')
 }).listen(8080)
